01bfb47bacc5b3f12e2c232319d5d169d9c2cf2f
RSurya99 <rafli.rfsp@gmail.com>
2022-02-27 15:18:52 +0700

    add comp-exercise-problem project and some notes

---
+++ comp-exercise-problem/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <h1>Your Upcoming Destinations</h1>
+    <div style="display: flex;">
+      <card v-for="location in locations" :key="location.name" :img-src="location.img" :city-name="location.name" :description="location.desc"></card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card,
+  },
+  data(){
+    return {
+      locations: [
+        {
+          name: 'moscow',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        },
+         {
+          name: 'rome',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`
+        },
+         {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
+          desc: `By the end of the 12th century, Paris had become the political, economic, religious, and cultural capital of France. Maurice de Sully undertook the construction of the Notre Dame Cathedral at its eastern extremity.`
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  font-family: 'NTR', sans-serif;
+  background: #eee;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
---
+++ comp-exercise-problem/src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="location-contain">
+      <div class="locations">
+        <div class="place">
+          <img :src="imgSrc" width="235" height="300"/>
+          <h2>{{ cityName }}</h2>
+          <p>{{ description }}</p>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['imgSrc', 'cityName', 'description'],
+}
+</script>
+<style scoped>
+.location-contain {
+  display: flex;
+  justify-content: center;
+}
+
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 320px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+}
+
+.place h2 {
+  margin: 0;
+  text-align: center;
+}
+
+.place img {
+  margin: 10px;
+  align-self: center;
+}
+</style>
